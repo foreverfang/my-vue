9b488b167ccd0e3126cb47235586fea59bdea581
gaojianbing <jbgao@isoftstone.com>
2021-02-08 17:24:22 +0800

    20210208代码提交

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">el-input组件</router-link> |
+      <router-link to="/about">element组件</router-link> |
       <router-link to="/print">nb打印</router-link>
     </div>
     <router-view/>
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,18 +2,27 @@
   <div class="about">
     <h1>This is an about page</h1>
     <el-button @click="onreset">重置</el-button>
+    <el-input v-model="form.input"></el-input>
     <KeywordInput v-model.trim="queryParams.keyword" @enter="searchList"></KeywordInput>
+    <ElCascader></ElCascader>
   </div>
 </template>
 <script>
 import KeywordInput from "@/components/keywordInput.vue"
+import ElCascader from "@/components/elCascader.vue"
 export default {
     data(){
         return {
             queryParams: {
                 keyword: ""
+            },
+            form: {
+                input: ''
             }
         };
+    },
+    created(){
+        // this.form.input = ''
     },
     methods: {
         searchList() {
@@ -25,7 +34,8 @@ export default {
         }
     },
     components: {
-        KeywordInput
+        KeywordInput,
+        ElCascader
     }
 
 }
